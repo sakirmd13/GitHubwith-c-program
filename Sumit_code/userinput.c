5702e29002f2746d7d733d5dc7f23f8f7f1355f2
ArenaMirror03 <officialsumitsharma03@gmail.com>
2024-11-15 17:59:45 +0530

    user name

--- Sumit_code/userinput.c
+++ Sumit_code/userinput.c
@@ -1,30 +1,25 @@
-#include <stdio.h>
+# include<stdio.h>
 
-void getUserDetails(char *firstName, char *lastName, char *address, char *mobileNumber) {
-    printf("Enter your first name: ");
-    scanf("%s", firstName);
-    
-    printf("Enter your last name: ");
-    scanf("%s", lastName);
-    
-    printf("Enter your address: ");
-    getchar(); 
-    fgets(address, 100, stdin);
-    
-    printf("Enter your mobile number: ");
-    scanf("%s", mobileNumber);
+void userdetails();
+char firstname; 
+char lastname; 
+char address; 
+char mobilenumber;
+
+int main(){
+    userdetails();
 }
 
 
 
-int main() {
-    char firstName[50];
-    char lastName[50];
-    char address[100];
-    char mobileNumber[15];
-    
-    getUserDetails(firstName, lastName, address, mobileNumber);
-   
-    
-    return 0;
+
+void userdetails(){
+    printf("Enter your first name: \n ");
+    scanf("%s",&firstname);
+    printf("Enter your last name: \n ");
+    scanf("%s",&lastname);
+    printf("Enter your address: \n ");
+    scanf("%s",&address);
+    printf("Enter your mobile number: \n ");
+    scanf("%s",&mobilenumber);
 }

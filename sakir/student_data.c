6c8e69609ca3f00c5384b8e69105711f594fa2ce
sakirmd13 <mds079475@gmail.com>
2024-11-15 18:30:24 +0530

    no changes

---
+++ sakir/student_data.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    char first, second, email;
+    int mobile;
+
+    printf("Enter your first  name: \n");
+    scanf("%s", &first);
+
+    printf("Enter your last name: \n ");
+    scanf("%s", &second);
+
+    printf("Enter your email: \n ");
+    scanf("%s", &email);
+
+    printf("Enter your mobile number: \n");
+    scanf("%d", &mobile);
+
+    return 0;
